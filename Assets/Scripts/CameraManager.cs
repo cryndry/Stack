b357db27056246167942f5093d5d6754e2ae302b
cryndry <ismailkaraagac94@gmail.com>
2025-11-28 09:19:50 +0300

    camera movement is fixed

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -6,7 +6,6 @@ class CameraManager : LazySingleton<CameraManager>
 
     public void MoveCameraUp(float offset)
     {
-        // TODO: It should'n be 1.5, it should be calculated according to the camera angle.
-        mainCamera.transform.position += offset / 1.5f * (mainCamera.transform.up - mainCamera.transform.forward);
+        mainCamera.transform.position += offset * Vector3.up;
     }
 }
